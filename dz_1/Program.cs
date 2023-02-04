c54d35501d31e17b9121a59577fdb840f8315aee
Evgeniy <evgeniyklimow@gmail.com>
2023-02-04 14:56:48 +0300

    add consoles dz projects

--- dz_1/Program.cs
+++ dz_1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ")
+int num = int.Parse(Console.ReadLine());
+int result = (num / 100) * 10 + num % 10;
+
+Console.WriteLine ($"{num} -> {result}");
+Console.WriteLine ($"{num} -> {num/100}{num%10}");
